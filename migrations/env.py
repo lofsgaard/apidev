76d5e442c6212a768ac888411c8edaa15508f89f
Andreas Løfsgaard <andreas@lofsgaard.com>
2024-02-13 21:20:10 +0100

    fix pw in alembic.ini

--- migrations/env.py
+++ migrations/env.py
@@ -1,4 +1,6 @@
 import asyncio
+from dotenv import load_dotenv, find_dotenv
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import pool
@@ -10,9 +12,17 @@ from alembic import context
 
 from auth.models import Users
 
+load_dotenv(find_dotenv())
+
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option('sqlalchemy.url', DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
